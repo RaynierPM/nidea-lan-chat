4831242a7853c848c32eb8fb7cfcf59ea4e3c90a
RaynierPM <raynier.p@blockchanger.io>
2025-07-28 13:40:54 -0400

    [Feat] New pop-over component

---
+++ electron/renderer/src/components/common/pop-over.tsx
@@ -0,0 +1,55 @@
+import React, { ReactNode } from "react";
+import { cn } from "../../utils/cn.util";
+
+type PopOverProps = {
+  position?: 'top' | 'bottom' | 'left' | 'right';
+  children?: React.ReactNode;
+  className?: string;
+  onOpenChange?: (isOpen: boolean) => void;
+  isOpen?: boolean;
+  anchorElement?: ReactNode | null;
+  triggerElement?: ReactNode | null;
+}
+
+const positionClasses = {
+  top: 'absolute bottom-full left-1/2 -translate-x-12',
+  bottom: 'absolute top-full left-1/2 -translate-x-12',
+  left: 'absolute right-full top-1/2 -translate-y-1/2',
+  right: 'absolute left-full top-1/2 -translate-y-1/2'
+}
+
+export function PopOver({
+  position = 'bottom',
+  children,
+  className = '',
+  onOpenChange,
+  isOpen = false,
+  triggerElement
+}: PopOverProps) {
+  const positionClass = positionClasses[position] || positionClasses.bottom;
+  
+  return (
+    <div className={[
+      "relative",
+    ].join(" ")}>
+      <div 
+        onClick={() => {onOpenChange?.(!isOpen)}}
+        onKeyDown={(e) => {
+          if (e.key === 'Escape') {
+            onOpenChange?.(false);
+          }
+        }}
+      >
+        {triggerElement}
+      </div>
+      <div className={cn(
+        className,
+        positionClass,
+         " z-50 ", 
+         isOpen ? "block" : "hidden"
+      )}>
+        {children}
+      </div>
+    </div>
+  )
+}
--- electron/renderer/src/components/features/room/DisconnectModal.tsx
+++ electron/renderer/src/components/features/room/DisconnectModal.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Modal } from "../../components/Modal";
+import { Modal } from "../../Modal";
 
 interface DisconnectModalProps {
   open: boolean;
